9065542f1552ef2b1d89542597445b00bf0d0969
Isaías Reyes León <elreyieon@Isaiass-MacBook-Air.local>
2025-08-06 15:38:01 -0600

    fix issues to deploy in gh pages

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path="/portfolio" element={<Portfolio />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/resume" element={<Resume />}></Route>
-        <Route path="/React-Portfolio" element={<About />}></Route>
+        <Route path="/cliniia" element={<About />}></Route>
         <Route path="/" element={<About />}></Route>
       </Routes>
       <Footer />
